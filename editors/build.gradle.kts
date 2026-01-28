4eb3e252be4fa6cad2f252d0ef5c8bf454b979da
Daniel Orr <orrdaniel73@gmail.com>
2026-01-28 15:18:57 +0000

    feat: worldedit

--- editors/build.gradle.kts
+++ editors/build.gradle.kts
@@ -6,4 +6,5 @@ dependencies {
     implementation("net.minestom:minestom:$minestom_version")
     api(project(":blueprint:minestom"))
     api(project(":commands"))
+    api("io.github.openminigameserver.worldedit:MinestomWorldEdit:1.4")
 }
