a737a2a01f4b3aefeeac17321f3f42b7481102d6
Daniel Orr <orrdaniel73@gmail.com>
2026-01-29 21:38:34 +0000

    chore: remove worldedit from blueprint directly

--- editors/build.gradle.kts
+++ editors/build.gradle.kts
@@ -6,5 +6,4 @@ dependencies {
     implementation("net.minestom:minestom:$minestom_version")
     api(project(":blueprint:minestom"))
     api(project(":commands"))
-    api("io.github.openminigameserver.worldedit:MinestomWorldEdit:1.4")
 }
