3e6b5d3c54c18bd554ed4d0cdf3f85deb208a62f
Daniel Orr <orrdaniel73@gmail.com>
2026-03-28 20:26:23 +0000

    chore: update codex

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     api("com.mojang:datafixerupper:7.0.14")
-    api("net.mcbrawls:codex:2.0.0")
+    api("net.mcbrawls:codex:2.0.1")
     api("org.joml:joml:1.10.8")
     api("org.slf4j:slf4j-api:2.0.17")
     api("net.kyori:adventure-nbt:4.25.0")
